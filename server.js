a68222bcd98bafa23eb80079d5427b3532be88dc
Yuusarii <yalhindi7@gmail.com>
2022-11-27 19:28:11 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+
+// Setup the file for integration with node.js / Express & add the frontend pages
+
+
+
+// We need a method to 
+// Given: Array of courses the user already finished
+// Result: Array with the remaining courses left for him. 
+// Note: filter all courses based on standing level and pre-requisite requirements fulfillled or not.
+
+
+// Method 2:
+// Given student GPA
+// Decide Number of hours allowed
+
+
+// Method 3
+// Generate tables
+
+
+// Method 4
+// Check a table partial solution does not violate any constraint rules
+
+
+// Method 5: 
+// Check if a table is_goal (Complete and Correct)
+
+// Method 6:
+// Given a complete table, generate a rating for how "good" that table is
+
+
+// Method 7 - AC-3 & MRV Algorithims
+// (Implemnting them for better tree-search efficincy with Arc-consistency and Forward Checking)
