ebf6a7e1116f38e91923821b52bb42070694c08d
Yuusarii <yalhindi7@gmail.com>
2022-11-29 03:12:57 +0300

    DFS Added

--- server.js
+++ server.js
@@ -10,9 +10,13 @@
 
 
 // Method 2:
-// Given student GPA
+// Given student GPA, and term status (summer or regular)
 // Decide Number of hours allowed
 
+
+// Output after method 2 should be array of (filtered) courses left for the student to take. Every 
+// course in the format of a course object
+// and an array "Hours" of size two. Hours[0] = Min Hours allowed, Hours[1] = Max hours allowed 
 
 // Method 3
 // Generate tables
@@ -50,9 +54,13 @@ class TableCSP {
         for (i=0; i<length(state); i++)
         hoursLoad += state[i].credit
         
-        if ((hoursLoad + newCourse.credit) > this.maxHours)
+
+        if (!(newCourse in state.Courses)) {
+            if ((hoursLoad + newCourse.credit) > this.maxHours)
             return false
         return True
+        }
+        return False
 
     }
 }
@@ -82,8 +90,34 @@ class Course {
     get lab() {
         return this.lab
     }
+}
 
 
+TableDFS(problem, solution, depthGoal) {
+    // Problem is an instance of TableCSP 
+    // Check if a problem is complete, i.e: len == the depthGoal
+    // if problem is not complete, assign a random new Course to the solution
+    // Check solution is consistent 
 
+    if (length(solution) == depthGoal)
+        return solution
+    solution_Copy = JSON.parse(JSON.stringify(solution))
+    problem_Copy = JSON.parse(JSON.stringify(problem))
+
+    // Assign a new random value to Sol_copy
+    for (CourseItem in domain_Copy) {
+        if (problem.is_consistent(solution_Copy, CourseItem)) {
+            // update copied problem domain
+            index = Array.prototype.indexOf(problem_Copy.Course, CourseItem)
+            problem_Copy.Course.splice(index, 1)
+
+            // Append CourseItem to Solution_Copy
+            solution_Copy.push(CourseItem)
+            
+            return TableDFS(problem_Copy, solution_Copy, depthGoal)
+        }
+    }
 
 }
+
+
