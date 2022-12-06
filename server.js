81bdc4514578d569d2eb4b8f850b28cb45564b61
Yuusarii <yalhindi7@gmail.com>
2022-12-06 08:30:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -41,17 +41,16 @@
 
 const user = {
     name: "Rakan", 
-    GPA: 3.1,
+    GPA: 2.7,
     standing: "Junior",
     partTimeHours: 0
 }
 
 class TableCSP {
-    constructor(Courses, hours, rules) {
+    constructor(Courses, hours) {
         this.Courses = Courses
         this.minHours = hours[0]
         this.maxHours = hours[1]
-        this.rules = rules
     }
 }
 
@@ -68,21 +67,7 @@ class Course {
         this.importance = 0
     }
 
-    get name() {
-        return this.name
-    }
-    get credit() {
-        return this.credit
-    }
-    get difficulty() {
-        return this.difficulty
-    }
-    get standingLevel() {
-        return this.standingLevel
-    }
-    get lab() {
-        return this.lab
-    }
+
 }
 
 function is_consistent(problem, solution, newitem) {
@@ -107,7 +92,7 @@ function minHoursAchieved(problem, solution) {
         hoursLoad += solution[i].credit
     }
 
-    if (hoursLoad > problem.minHours) 
+    if (hoursLoad >= problem.minHours) 
         return true
     else
         return false
@@ -133,21 +118,25 @@ function generate_Tables(problem, solution, summer) {
     final_result = evaluate_Table(solution_result)
 
     console.log("FINAL RESULT\nTABLE ONE:")
-    console.log(table_toPrint(final_result[0]))
+    table_toPrint(final_result[0])
     console.log("TABLE TWO:")
-    console.log(table_toPrint(final_result[1]))
+    table_toPrint(final_result[1])
     console.log("TABLE THREE:")
-    console.log(table_toPrint(final_result[2]))
+    table_toPrint(final_result[2])
 
 
 }
 
 
 function table_toPrint(arr) {
-    for (let j=0; j<arr.length; j++) 
-    names += arr[j].name + " , "
+    hours = 0
+    let names = ""
+    for (let j=0; j<arr.length; j++) {
+        names += arr[j].name + ", "
+        hours += arr[j].credit
+    }
     let holder = Math.round(getValue(arr) * 1000 ) / 1000
-    console.log(" With Courses: " + names + " Evaluation is:" + holder)
+    console.log("Length : "+ arr.length + " || Hours : " + hours + "\nCourses: " + names + " || Table Score = " + holder)
 }
 
 function TableDFS(problem, solution, depthGoal) {
@@ -156,7 +145,7 @@ function TableDFS(problem, solution, depthGoal) {
     let problem_Copy = JSON.parse(JSON.stringify(problem))
 
     if (solution_Copy.length === depthGoal) {
-        if (minHoursAchieved) {
+        if (minHoursAchieved(problem, solution_Copy) === true) {
             solution_result.push(solution_Copy)
             return true
         }
@@ -170,15 +159,6 @@ function TableDFS(problem, solution, depthGoal) {
 
     if (solution_Copy.length === 0) {
         problem_Copy.Courses = DFSstart(problem_Copy)
-        let root = getHighestPrio(problem)
-                solution_Copy.push(root)
-        var ind = problem_Copy.Courses.findIndex( x => 
-            x.name === root.name
-            );
-
-            if (ind !== -1) {
-                problem_Copy.Courses.splice(ind, 1)
-            }
 
 
         let prioHolder = getHighestPrio(problem_Copy)
@@ -349,27 +329,28 @@ function getHighestPrio(problem) {
 function GPA_Range(user) {
     let range = []
     if (user.GPA >= 3.5) {
-        range[0] = 25
-        range[1] = 40
+        range[0] = 10
+        range[1] = 36
 
     }
     else if (user.GPA >= 3) {
-        range[0] = 41
-        range[1] = 53
+        range[0] = 35
+        range[1] = 42
     }
     else if (user.GPA >= 2.5) {
-        range[0] = 54
-        range[1] = 65
+        range[0] = 41
+        range[1] = 47
     }
     else {
-        range[0] = 66
-        range[1] = 80
-    }
-
+        range[0] = 48
+        range[1] = 55
+    } 
 
 
     if (user.partTimeHours > 0) {
         range[1] += Math.ceil(user.partTimeHours*1.5)
+        if (user.GPA >= 3.5)
+            range[1] += 3.5
     }
     return range
 
@@ -387,30 +368,27 @@ function evaluate_Table(results) {
     GPA_max = GPA_Range(user)[1]
 
     for (let i=0; i<results.length; i++) {
-        let names = ""
-        for (let j=0; j<results[i].length; j++) 
-            names += results[i][j].name + " , "
-        let holder = Math.round(getValue(results[i]) * 1000 ) / 1000
-         console.log("Table " + i + " With Courses: " + names + " Evaluation is:" + holder)
+        let holder = Math.round(getValue(results[i]) * 10000 ) / 10000
+        if (results[i].length > 5) {
+            console.log("Table " + i)
+            table_toPrint(results[i])
+        }
 
+        
         if (holder >= GPA_min && holder <= GPA_max) {
             if (holder > max1) {
                 max1 = holder
                 max1Table = results[i]
             }
-            else if (holder > max2) {
+            else if (holder > max2 && holder !== max1 && (max1-holder) > 1.1) {
                 max2 = holder
                 max2Table = results[i]
             }
-            else if (holder > max3) {
+            else if (holder > max3 && holder !== max1 && holder !== max2 && (max2-holder) > 1.1) {
                 max3 = holder
                 max3Table = results[i]
             }
         } 
-        // check user.gpa to determine the range
-        // if in range, accept it and update Max1-3 if needed
-        // else, reject the table and continue to next one
-
     }
     max3_result = [max1Table, max2Table, max3Table]
     return max3_result
@@ -426,7 +404,7 @@ function getValue(table) {
         if (table[j].importance > 1) {
             score += 10
         }
-        score -= (table[j].credit*table[j].difficulty)/0.8
+        score -= (table[j].credit*table[j].difficulty)/2.5
         totalHours += table[j].credit 
         if (table[j].lab == true)
             score -= 3
@@ -437,106 +415,6 @@ function getValue(table) {
     return score
 }
 
-
-var c1 = {
-    name: "ICS102",
-    credit: 4,
-    difficulty: "Easy",
-    standingLevel: "Freshmen",
-    lab: false
-}
-
-var c2 = {
-    name: "ICS202",
-    credit: 3,
-    difficulty: "Hard",
-    standingLevel: "Freshmen",
-    lab: true
-}
-
-var c3 = {
-    name: "ICS203",
-    credit: 4,
-    difficulty: "Hard",
-    standingLevel: "Freshmen",
-    lab: true
-}
-
-var c4 = {
-    name: "ICS333",
-    credit: 4,
-    difficulty: "Medium",
-    standingLevel: "Freshmen",
-    lab: false
-}
-
-var c5 = {
-    name: "ICS345",
-    credit: 2,
-    difficulty: "Easy",
-    standingLevel: "Freshmen",
-    lab: true
-}
-
-var c6 = {
-    name: "ICS410",
-    credit: 4,
-    difficulty: "Hard",
-    standingLevel: "Freshmen",
-    lab: true
-}
-
-var c7 = {
-    name: "ICS442",
-    credit: 3,
-    difficulty: "Hard",
-    standingLevel: "Freshmen",
-    lab: true
-}
-
-var c8 = {
-    name: "ICS400",
-    credit: 1,
-    difficulty: "Hard",
-    standingLevel: "Freshmen",
-    lab: true
-}
-
-var c9 = {
-    name: "ICS1302",
-    credit: 2,
-    difficulty: "Easy",
-    standingLevel: "Freshmen",
-    lab: false
-}
-var c10 = {
-    name: "ICS1202",
-    credit: 2,
-    difficulty: "Easy",
-    standingLevel: "Freshmen",
-    lab: false
-}
-var c11 = {
-    name: "ICS1012",
-    credit: 3,
-    difficulty: "Easy",
-    standingLevel: "Freshmen",
-    lab: true
-}
-var c12 = {
-    name: "ICS2102",
-    credit: 4,
-    difficulty: "Easy",
-    standingLevel: "Freshmen",
-    lab: false
-}
-var c13 = {
-    name: "ICS2102",
-    credit: 3,
-    difficulty: "Easy",
-    standingLevel: "Freshmen",
-    lab: false
-}
 
 
 var course1 = {
@@ -652,9 +530,8 @@ var course12 = {
 
 ExampleHours = [12, 19]
 ExampleCourses = [course1,course2,course3,course4,course5,course6,course7,course8,course9,course10,course11]
-ExampleRules = []
 
-ExampleProblem = new TableCSP(ExampleCourses, ExampleHours, ExampleRules)
+ExampleProblem = new TableCSP(ExampleCourses, ExampleHours)
 
 
 
@@ -662,6 +539,3 @@ ExampleProblem = new TableCSP(ExampleCourses, ExampleHours, ExampleRules)
 generate_Tables(ExampleProblem, ExampleCourses, false)
 
 
-
-console.log("Solutions:" + solution_result.length)
-//console.log(solution_result)
